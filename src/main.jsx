1a58034db718855f379cc7356ffada9ded5966da
Bronze14 <marcindadek@gmail.com>
2023-05-20 19:40:44 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/main.scss'
+import './styles/css/main.css'
 import { Provider } from 'react-redux'
 import { store } from './Store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
